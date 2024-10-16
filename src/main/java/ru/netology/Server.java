754a9cfb48d1ff5b177cd562502b62daf05bd48e
Katerina-nik <eka-pukhti@yandex.ru>
2024-10-16 20:54:05 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        int port = 8089;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("New connection accepted");
+
+                    final String name = in.readLine();
+
+                    System.out.printf("Hi %s, your port is %d%n", name, clientSocket.getPort());
+                }
+            }
+        }
+    }
+}
+
